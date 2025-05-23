e8207a3299d4de8d2ec86c207fa0a618f5370060
zp4rker <iam@zp4rker.com>
2025-05-23 10:08:50 +1000

    add posts listing page

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,10 @@
+const moment = require("moment")
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.setInputDirectory("content")
     eleventyConfig.setIncludesDirectory("../_includes")
+
+    eleventyConfig.addFilter("date", function(date) {
+        return moment(date).format("YYYY-MM-DD")
+    })
 }
