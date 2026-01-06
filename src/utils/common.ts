5733f94d01170d5fe83f721b1cb4c9643e90e337
abulujayn <zaeem@parkar.au>
2026-01-07 00:40:03 +1100

    convert common.js to typescript and fix all linting issues

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,7 +2,7 @@ import moment from 'moment-hijri';
 
 moment.locale("en-us");
 
-export function formatDate(rawDate) {
+export function formatDate(rawDate: Date): string {
     const date = moment(rawDate);
     const year = date.year();
     const month = date.month() + 1;
@@ -10,7 +10,7 @@ export function formatDate(rawDate) {
     return `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
 }
 
-export function formatHijri(rawDate, date_shift) {
+export function formatHijri(rawDate: Date, date_shift?: number): string {
     const date = moment(rawDate);
     if (date_shift) {
         date.add(date_shift, 'iDate');
@@ -21,10 +21,10 @@ export function formatHijri(rawDate, date_shift) {
     return `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
 }
 
-export function filterTags(tags) {
+export function filterTags(tags?: string[]): string[] {
     return (tags || []).filter(tag => !["posts", "dunyā", "dīn"].includes(tag));
 }
 
-export function tagsOnly(tags) {
+export function tagsOnly(tags?: string[]): string[] {
     return (tags || []).filter(tag => !["posts", "all"].includes(tag));
 }
